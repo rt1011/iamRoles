e2109c47ee5075e5f20584e16ebc0cce42580d2d
rt1011 <51536334+rt1011@users.noreply.github.com>
2025-07-24 00:16:59 -0400

    Update updatedSQL.sql

--- updatedSQL.sql
+++ updatedSQL.sql
@@ -1,27 +1,17 @@
 WITH assumed_roles AS (
   SELECT 
-    eventID AS assume_event_id,
-    eventTime AS assume_time,
     responseElements.assumedRoleUser.arn AS session_arn,
-    userIdentity.arn AS human_actor,
-    userIdentity.type AS actor_type,
-    userIdentity.sessionContext.sessionIssuer.arn AS assumed_role_arn
+    userIdentity.arn AS human_actor
   FROM a
   WHERE eventName = 'AssumeRole'
-    AND day BETWEEN DATE '2025-06-01' AND DATE '2025-06-10'
+    AND day BETWEEN '2025/06/04' AND '2025/06/04'
 ),
 
 iam_creations AS (
   SELECT 
-    eventTime,
-    eventID,
-    eventName,
-    userIdentity.principalId,
     userIdentity.arn AS acting_session_arn,
-    userIdentity.sessionContext.sessionIssuer.arn AS issued_role_arn,
-    requestParameters,
-    sourceIPAddress,
-    awsRegion,
+    eventName,
+    eventTime,
     accountId
   FROM a
   WHERE eventsource = 'iam.amazonaws.com'
@@ -30,22 +20,17 @@ iam_creations AS (
         'PutRolePolicy', 'PutUserPolicy', 'PutGroupPolicy',
         'AttachRolePolicy', 'AttachUserPolicy', 'AttachGroupPolicy'
     )
-    AND day BETWEEN DATE '2025-06-01' AND DATE '2025-06-10'
+    AND day BETWEEN '2025/06/04' AND '2025/06/04'
 )
 
 SELECT 
   i.eventTime,
   i.eventName,
-  i.accountId,
-  i.sourceIPAddress,
-  i.awsRegion,
-  i.requestParameters,
   i.acting_session_arn,
-  a.human_actor AS true_actor,
-  a.actor_type,
-  a.assume_time AS role_assumed_at
+  a.session_arn,
+  a.human_actor
 FROM iam_creations i
 LEFT JOIN assumed_roles a
-  ON i.acting_session_arn = a.session_arn
-  AND i.eventTime BETWEEN a.assume_time AND a.assume_time + interval '1' hour
-ORDER BY i.eventTime DESC;
+  ON i.acting_session_arn LIKE CONCAT('%', a.session_arn)
+ORDER BY i.eventTime DESC
+LIMIT 50;
